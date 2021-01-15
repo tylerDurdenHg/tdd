791df37f7279854ca7c72ccf26ffb79a0250f522
Tyler Durdenhg <you@example.com>
2021-01-15 17:54:07 +0300

    xys

--- src/test/java/org/example/ShapeTest.java
+++ src/test/java/org/example/ShapeTest.java
@@ -11,7 +11,7 @@ class ShapeTest {
         Shape shape = new Shape();
         int corners = shape.getCorners();
         assertEquals(4,  corners);
-        System.out.println("test 2 ");
+        System.out.println("test 1 ");
     }
 
 }
