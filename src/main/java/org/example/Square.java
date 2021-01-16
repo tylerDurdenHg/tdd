3a0b984a7d980460575d188628d7042ccb1f1734
tylerDurdenHg <tylerdurdenhg@hotmail.com>
2021-01-16 12:11:12 +0300

    Update Square.java
    for test

--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -4,10 +4,11 @@ public class Square {
     private int factor = 2 ;
 
     public long getSquare(int i) {
+        // hi
         return (long) Math.sqrt(i);
     }
 
     public long multiplyTwo(int i) {
-        return i * factor;
+        return i * 211;
     }
 }
